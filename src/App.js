4ed921dd281b67618492669b38b5bd55b3fb388a
Masayuki Usui <usui-masayuki394@g.ecc.u-tokyo.ac.jp>
2023-04-30 13:59:13 +0900

    Improve layout

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import "github-markdown-css";
 
 import { useState } from "react";
 import ReactMarkdown from "react-markdown";
@@ -10,12 +11,12 @@ import { dark } from "react-syntax-highlighter/dist/esm/styles/prism";
 export default function App() {
   // States
   const [data, setData] = useState([
-    ["", "", ""],
+    ["```javascript\nlet x = 3;\nconcole.log(x);\n```", "- apple\n- banana\n- pen", "My favorite food:\n1. sushi\n   1. maguro\n   2. ebi\n2. steak"],
     ["", "", ""]
   ]);
 
-  const [rowHeight, setRowHeight] = useState(5);
-  const [columnWidth, setColumnWidth] = useState(5);
+  const [rowHeight, setRowHeight] = useState(10);
+  const [columnWidth, setColumnWidth] = useState(20);
 
   const [rowNum, setRowNum] = useState(2);
   const [colNum, setColNum] = useState(3);
@@ -108,95 +109,101 @@ export default function App() {
 
   return (
     <div className="App">
-      <ul>
-        <li>
-          <label>
-            The number of rows:
-            <input type="number" value={rowNum} onChange={handleRowNum} />
-          </label>
-        </li>
-        <li>
-          <label>
-            The number of columns:
-            <input type="number" value={colNum} onChange={handleColNum} />
-          </label>
-        </li>
-        <li>
-          <label>
-            The height of a row:
-            <input
-              type="number"
-              value={rowHeight}
-              onChange={(e) => setRowHeight(e.target.value)}
-            />
-          </label>
-        </li>
-        <li>
-          <label>
-            The width of a column:
-            <input
-              type="number"
-              value={columnWidth}
-              onChange={(e) => setColumnWidth(e.target.value)}
-            />
-          </label>
-        </li>
-      </ul>
-      <table>
-        <tbody>
-          {data.map((row, rowIdx) => (
-            <tr>
-              {row.map((cell, colIdx) => (
-                <td>
-                  <textarea
-                    rows={rowHeight}
-                    cols={columnWidth}
-                    value={cell}
-                    onChange={makeHandler(rowIdx, colIdx)}
-                  />
-                </td>
-              ))}
-            </tr>
-          ))}
-        </tbody>
-      </table>
       <div>
-        <ReactMarkdown
-          rehypePlugins={[rehypeRaw]}
-          remarkPlugins={[remarkGfm]}
-          components={{
-            code({ node, inline, className, children, ...props }) {
-              const match = /language-(\w+)/.exec(className || "");
-              return !inline && match ? (
-                <Prism
-                  {...props}
-                  children={String(children).replace(/\n$/, "")}
-                  style={dark}
-                  language={match[1]}
-                  PreTag="div"
-                />
-              ) : (
-                <code {...props} className={className}>
-                  {children}
-                </code>
-              );
-            }
-          }}
-        >
-          {code}
-        </ReactMarkdown>
+        <div className='centered'>
+          <div id="container">
+            <div id="item1">
+              <label>
+                The number of rows: <input type="number" value={rowNum} onChange={handleRowNum} />
+              </label>
+            </div>
+            <div id="item2">
+              <label>
+                The number of columns: <input type="number" value={colNum} onChange={handleColNum} />
+              </label>
+            </div>
+            <div id="item3">
+              <label>
+                The height of a row: <input type="number" value={rowHeight} onChange={(e) => setRowHeight(e.target.value)} />
+              </label>
+            </div>
+            <div id="item4">
+              <label>
+                The width of a column: <input type="number" value={columnWidth} onChange={(e) => setColumnWidth(e.target.value)} />
+              </label>
+            </div>
+          </div>
+        </div>
       </div>
-      <textarea rows={10} cols={20} value={code} readOnly />
-      <button
-        onClick={() =>
-          navigator.clipboard.writeText(code).then(
-            () => alert("Successfully copied"),
-            () => alert("Failed to copy")
-          )
-        }
-      >
-        Copy
-      </button>
+      <div>
+        <div className='centered'>
+          <table>
+            <tbody>
+              {data.map((row, rowIdx) => (
+                <tr>
+                  {row.map((cell, colIdx) => (
+                    <td>
+                      <textarea
+                        rows={rowHeight}
+                        cols={columnWidth}
+                        value={cell}
+                        onChange={makeHandler(rowIdx, colIdx)}
+                      />
+                    </td>
+                  ))}
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      </div>
+      <div className='markdown-body'>
+        <div className='centered'>
+          <ReactMarkdown
+            rehypePlugins={[rehypeRaw]}
+            remarkPlugins={[remarkGfm]}
+            components={{
+              code({ node, inline, className, children, ...props }) {
+                const match = /language-(\w+)/.exec(className || "");
+                return !inline && match ? (
+                  <Prism
+                    {...props}
+                    children={String(children).replace(/\n$/, "")}
+                    style={dark}
+                    language={match[1]}
+                    PreTag="div"
+                  />
+                ) : (
+                  <code {...props} className={className}>
+                    {children}
+                  </code>
+                );
+              }
+            }}
+          >
+            {code}
+          </ReactMarkdown>
+        </div>
+      </div>
+      <div>
+        <div className='centered'>
+          <div>
+            <textarea rows={10} cols={20} value={code} readOnly />
+          </div>
+          <div>
+            <button
+              onClick={() =>
+                navigator.clipboard.writeText(code).then(
+                  () => alert("Successfully copied"),
+                  () => alert("Failed to copy")
+                )
+              }
+            >
+              Copy
+            </button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
